98f2deebea79a23c7f66e76a643e098a6d94ce8e
dtdinidu7 <dtdinidu7@gmail.com>
2021-06-21 23:34:46 +0530

    new categories updated

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ function processData(data) {
     // console.log(data);
     // console.log(JSON.parse(data));
     data = JSON.parse(data);
-    var research_talk = data['research_talk'];
-    var competion = data['competition'];
+    var research_talks = data['research_talk'];
+    var competitions = data['competition'];
+    var conferences = data['conference'];
+    var workshops = data['workshop'];
 
     var finalArray = [];
 
     // adding talks to the final display array
-    research_talk.forEach((talk) => {
+    research_talks.forEach((talk) => {
         var typeTxt = '';
         if (talk['registration'] == '1') {
             typeTxt = 'Register';
@@ -21,7 +23,7 @@ function processData(data) {
             'date': talk['date'],
             'time': talk['time'],
             'description': talk['title'],
-            'type': "Talk",
+            'type': "Research Talk",
             'details': "Dummmy Details",
             'link_text': typeTxt,
             'link': talk['link'],
@@ -29,16 +31,62 @@ function processData(data) {
     });
 
     // adding competitions to the final display array
-    competion.forEach((competion) => {
+    competitions.forEach((competition) => {
+        var typeTxt = '';
+        if (competition['registration'] == '1') {
+            typeTxt = 'Register';
+        } else {
+            typeTxt = 'More Info';
+        }
 
         finalArray.push({
-            'date': competion['date'],
-            'time': competion['time'],
-            'description': competion['description'],
+            'date': competition['date'],
+            'time': competition['time'],
+            'description': competition['description'],
             'type': "Competition",
             'details': "Dummmy Details",
-            'link_text': 'more info',
-            'link': competion['link'],
+            'link_text': typeTxt,
+            'link': competition['link'],
+        });
+    });
+
+    // adding conferences to the final display array
+    conferences.forEach((conference) => {
+        var typeTxt = '';
+        if (conference['registration'] == '1') {
+            typeTxt = 'Register';
+        } else {
+            typeTxt = 'Live Stream';
+        }
+
+        finalArray.push({
+            'date': conference['date'],
+            'time': conference['time'],
+            'description': conference['topic'],
+            'type': "Conference",
+            'details': "Dummmy Details",
+            'link_text': typeTxt,
+            'link': conference['link'],
+        });
+    });
+
+    // adding conferences to the final display array
+    workshops.forEach((workshop) => {
+        var typeTxt = '';
+        if (workshop['registration'] == '1') {
+            typeTxt = 'Register';
+        } else {
+            typeTxt = 'More Info';
+        }
+
+        finalArray.push({
+            'date': workshop['date'],
+            'time': workshop['time'],
+            'description': workshop['topic'],
+            'type': "Work Shop",
+            'details': "Dummmy Details",
+            'link_text': typeTxt,
+            'link': workshop['link'],
         });
     });
 
@@ -57,7 +105,7 @@ function processData(data) {
         }
     });
 
-    console.log(finalArray);
+    // console.log(finalArray);
 
     fillTable(finalArray);
 }
@@ -71,7 +119,7 @@ function addRow(rowData) {
 
     row.insertCell(0).innerHTML = rowData['type'];
     row.insertCell(1).innerHTML = rowData['date'];
-    row.insertCell(2).innerHTML = rowData['time'];
+    row.insertCell(2).innerHTML = rowData['time'] ? rowData['time'] : "TBA";
     row.insertCell(3).innerHTML = rowData['description'];
     row.insertCell(4).innerHTML = '<a target="_blank" href=' + rowData['link'] + '>' + rowData['link_text'] + '</a>';
     row.insertCell(5).innerHTML = '<button type="button" onclick="">Additional Info</button>'
